c93c56d798d8f8b2db3025306b329f1a146e692c
Simon_lfr <simonryom@gmail.com>
2018-12-16 14:37:55 +0100

    final_finalFINAL

--- api.py
+++ api.py
@@ -6,22 +6,26 @@ def get_all_messages():
     djson = requests.get('http://localhost:9000/api/messages')
     json_dict = json.loads(djson.content.decode())
     msgs = json_dict.get('msgs')
+    print('API call: GET /messages')
     return msgs
 
 
 def get_user(usr_name, usr_pswd):
     response = requests.get('http://localhost:9000/api/user/' + usr_name +'?p=' + usr_pswd)
     dict = json.loads(response.content.decode())
+    print('API call: GET /user/' + usr_name +'?p=' + usr_pswd)
     if bool(dict):
         return dict.get('usr_id')
     else:
         return None
 
+
 def create_user(json):
-    djson = requests.post('http://localhost:9000/api/user', data=None, json=json)
-    return 1
+    print('API call: POST /user')
+    return requests.post('http://localhost:9000/api/user', data=None, json=json)
 
 
 def post_message(json):
+    print('API call: POST /messages')
     return requests.post('http://localhost:9000/api/messages', data=None, json=json)
 
--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
